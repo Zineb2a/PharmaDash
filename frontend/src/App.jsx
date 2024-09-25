5f82d710632d087b88ce09549f09bf86e1afe0d9
Zineb2a <zineb.alaoui663@gmail.com>
2024-09-25 14:46:25 -0400

    Navbar and Footer in construction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <Navbar />
+<   div>
+    <Navbar/>
+    <Footer/>
+    </div>
   )
 }
 
