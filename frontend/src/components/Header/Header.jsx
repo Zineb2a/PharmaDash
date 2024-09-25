951b3fccb0eab1be28da8e40fc2caed0aba461e7
Zineb2a <zineb.alaoui663@gmail.com>
2024-09-25 14:26:47 -0400

    added folders, header footer

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Get Your Medications Delivered Fast with PharmaDash</h2>
+                <p>Conveniently order prescription and over-the-counter medications from your local pharmacy. We deliver right to your doorstep, ensuring you get the care you need quickly and safely.</p>
+                <button>Browse Medications</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect, useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+
+
+const Navbar = () => {
+
+
+
+  return (
+    <div className='navbar'>
+  <img className='logo' src={assets.logo2} alt="logo" /> 
+      
+  
+      </div>
+ 
+  )
+}
+
+export default Navbar
