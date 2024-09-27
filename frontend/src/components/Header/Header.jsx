a1187d85db9ad8d49fb7a627f026cc9aeef471f0
HaifaaJanoudi <138940955+HaifaaJanoudi@users.noreply.github.com>
2024-09-26 22:27:58 -0400

    small change

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Your meds at your front porche, only a few clicks away!</h2>
+                <h2>Your meds at your front porch, only a few clicks away!</h2>
                 <p>Either you have a prescription or not, our main mission is to respond to your medical needs in a few steps only!</p>
                 <button>View Menu</button>
             </div>
