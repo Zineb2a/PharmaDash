9003519ec0380d7d8ea68eebad7a835f2e16cf9e
Yan P <y.pilon9@gmail.com>
2024-11-04 14:31:49 -0500

    setting up backend environment

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	server "pharmaDashServer/router"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	// Server
+	server, err := server.GetNewServer()
+	if err != nil {
+		log.Fatal("Could not start the server")
+	}
+
+	server.Router.Run(":3000")
+}
