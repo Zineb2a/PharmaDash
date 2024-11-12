7d61c2d72e57b77496b9d7983ec5f45f00f228ad
Hawraa Al-Adilee <hawraa.aladilee@gmail.com>
2024-11-12 16:04:27 -0500

    Revert "Back end branch"

--- backend/router/server.go
+++ backend/router/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"net/http"
 	"os"
-	appsystem "pharmaDashServer/app_system"
 	"pharmaDashServer/token"
 
 	"github.com/gin-gonic/gin"
@@ -16,7 +15,6 @@ type Server struct {
 	Router *gin.Engine
 	maker  *token.PasetoMaker
 	pool   *pgxpool.Pool
-	system *appsystem.System
 }
 
 func GetNewServer() (*Server, error) {
@@ -34,12 +32,10 @@ func GetNewServer() (*Server, error) {
 	}
 
 	router := gin.Default()
-	system := appsystem.InitializeSystem()
 	server := &Server{
 		Router: router,
 		maker:  tokenMaker,
 		pool:   pool,
-		system: system,
 	}
 
 	//mount routes here
@@ -58,7 +54,6 @@ func GetNewServer() (*Server, error) {
 			cart.POST("/add_cart_item", server.mustAuthChecker, server.addCartItem)
 			cart.POST("/remove_cart_item", server.mustAuthChecker, server.removeCartItem)
 			cart.POST("/cancel_cart", server.mustAuthChecker, server.cancelShoppingCart)
-			cart.POST("/delivery_quotation", server.mustAuthChecker, server.CreateDeliveryQuotation)
 
 		}
 	}
