db57f46fdaf1c15447b491ee5260324a8ce5532e
Hawraa Al-Adilee <hawraa.aladilee@gmail.com>
2024-11-23 16:43:14 -0500

    quotation request migrations

---
+++ backend/db/migrations/000004_create_quotation_requests_table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE quotation_requests (
+    quotation_id SERIAL PRIMARY KEY,
+    total_cost NUMERIC,
+    delivery_frequency TEXT,
+    destination TEXT,
+    special_handling TEXT,
+    insurance NUMERIC,
+    include_insurance BOOLEAN,
+    is_refused BOOLEAN,
+    cart_id INT
+);
