88819e26088ff14781db66d75ce804aa4b9b8b86
Hawa-Afnane Said <hawa21244@gmail.com>
2024-11-24 23:51:06 -0500

    Submit A Review component done

--- frontend/src/pages/Prescriptions/Prescriptions.jsx
+++ frontend/src/pages/Prescriptions/Prescriptions.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import LeaveAReview from "../../components/LeaveAReview/LeaveAReview";
 import "./Prescriptions.css";
 
 const Prescription = () => {
